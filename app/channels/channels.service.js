71b3960a14ee3d3038cf557f6391b3d203670b96
djl132 <djl132@case.edu>
2017-02-12 06:43:11 -0800

    Creating Channels SideBar: Will Debug Later
    Deleted unnecessary lines.

--- app/channels/channels.service.js
+++ app/channels/channels.service.js
@@ -33,15 +33,3 @@ angular
 
 
 
-
-
-
-
-
-angular.module('angularfireSlackApp')
-  .factory('Channels', function($firebaseArray){
-    var ref = firebase.database().ref('channels'); //when was this array created?/....
-    var channels = $firebaseArray(ref);
-
-    return channels;
-  });
