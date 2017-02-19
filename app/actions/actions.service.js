da0d594743b65e7439140c346bef3e9717adcaf1
djl132 <djl132@case.edu>
2017-02-19 12:23:42 -0800

    Debugging: Firebase Now reading Added firebase array of voters for
    - now to add functions
    
    when getting a firebase array, you must put stuff in then, if you want
    to use that firebase array.

--- app/actions/actions.service.js
+++ app/actions/actions.service.js
@@ -8,19 +8,23 @@ angular
   actions.upvote = function(message){ 
     var channelMessagesRef = firebase.database().ref('channelMessages');
     var messageRef = channelMessagesRef.child(message.channelId +'/' + message.$id);
-    var upvoteRef = messageRef.child('upvote').$loaded().then(function(ref){
-      return ref});
+    var upvoteRef = messageRef.child('upvote');
     console.log(upvoteRef)
-    var upvote = $firebaseArray(upvoteRef);
-        console.log(upvote)
-
     var voter = message.uid;
 
+    var upvote = $firebaseArray(upvoteRef);
+    
+    upvote.$loaded().then(function(ref){
+      ref.$add({voter: voter});
+    });
+    
+    console.log(upvote)
+  }
+
+    
 //    if(!upvote.$getRecord(voter)){////this needs to check whether or not the uid has already been added.
 //      if(message.downvote)
 //        message.downvote.$remove({voter :voter});
-      upvote.$add({voter :voter});
-  };
     
   actions.downvote = function(message){  
     var ref = firebase.database().ref('channelMessages').child(message.channelId +'/'+ message.$id + '/downvote' );
