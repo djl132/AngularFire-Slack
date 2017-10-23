ac51f3cd9932726f1fec969cd1fa1f3bf4b36a5d
DerekLin <djl132@case.edu>
2017-10-23 14:51:03 -0400

    changed package.json to work with bower

--- app/app.js
+++ app/app.js
@@ -125,7 +125,6 @@ angular
         //get channelName of the current channel using channelId
         //WHEN WAS THIS CHANNEL ID CREATED? UNLESS ITS SPECIFIED IN RULES.
         channelName: function($stateParams, Channels){
-          debugger;
           return '#' + Channels.$getRecord($stateParams.channelId).name;
         }
       }
